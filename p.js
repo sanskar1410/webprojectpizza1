5aaf5c5a181e3df3ae7c6e8c795a6d413046a696
Sanskar Gupta <sanskargupta92210@gmail.com>
2024-06-01 14:54:33 +0530

    Add files via upload

---
+++ p.js
@@ -0,0 +1,84 @@
+let navbar = document.querySelector('.header .flex .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    
+}
+
+function scrollvalue(){
+   var navbar= document.getElementById('headerflex');
+   var scroll = window.scrollY;
+   if(scroll < 300){
+      navbar.classList.remove('BgColourofnavbar');
+   }
+   else{
+      navbar.classList.add('BgColourofnavbar');
+   }
+}
+
+window.addEventListener('scroll' ,scrollvalue);
+
+let account = document.querySelector('.user-account');
+
+document.querySelector('#user-btn').onclick = () =>{
+   account.classList.add('active');
+}
+
+document.querySelector('#close-account').onclick = () =>{
+   account.classList.remove('active');
+}
+
+
+let myOrders = document.querySelector('.my-order');
+
+document.querySelector('#order-btn').onclick = () =>{
+   myOrders.classList.add('active');
+}
+
+document.querySelector('#close-order').onclick = () =>{
+   myOrders.classList.remove('active');
+}
+
+
+let cart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () =>{
+   cart.classList.add('active');
+}
+
+document.querySelector('#close-cart').onclick = () =>{
+   cart.classList.remove('active');
+}
+
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    myOrders.classList.remove('active');
+   cart.classList.remove('active');
+}
+
+let slides = document.querySelectorAll('.home-bg .home .slide-container .slide');
+let index =0;
+
+function next() {
+   slides[index].classList.remove('active');
+   index= (index+1)%slides.length;
+   slides[index].classList.add('active');
+}
+
+function prev() {
+   slides[index].classList.remove('active');
+   index= (index-1 +slides.length)%slides.length;
+   slides[index].classList.add('active');
+}
+
+
+
+let faqindi = document.querySelectorAll('.faq .faq-container .faqindi');
+
+faqindi.forEach(acco =>{
+   acco.onclick = () =>{
+      faqindi.forEach(remove => remove.classList.remove('active'));
+      acco.classList.add('active');
+   }
+});
